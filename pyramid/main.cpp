2cc2fdc33611303474ea041dfa80c9662c52584c
Vinicius Roland <>
2024-04-14 21:25:24 -0300

    move folder

--- pyramid/main.cpp
+++ pyramid/main.cpp
@@ -7,7 +7,7 @@
 #include "../glm/gtc/matrix_transform.hpp"
 #include "../glm/gtc/type_ptr.hpp"
 
-#include "stb_image.h"
+#include "../stb/stb_image.h"
 #include "helpers/camera.h"
 
 #include <iostream>
